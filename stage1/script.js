2e32f33e6f657ec2172f6784886e93018aa126be
Mide Giwa <68765467+MideGiwa@users.noreply.github.com>
2023-09-10 21:54:02 +0100

    Create script.js

---
+++ stage1/script.js
@@ -0,0 +1,12 @@
+         // Get current date  
+          const currentDate = new Date();  
+          const currentDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(currentDate);  
+          document.getElementById("currentDay").textContent = currentDay;  
+  
+          function updateUTCTime() {  
+              //const currentUTC = new Date();  
+              //const currentTime = currentUTC.toISOString().split('T')[1].replace('Z', '');  
+              document.getElementById("currentUTC").textContent = Date.now();  
+          }  
+  
+          setInterval(updateUTCTime, 1000);
